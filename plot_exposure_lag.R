f9d74ba276660addd54394271399ec19c664a898
Amruta <amrutaumakant.mahakalkar@mail.polimi.it>
2024-09-06 01:47:33 +0200

    revised lag treatment

--- plot_exposure_lag.R
+++ plot_exposure_lag.R
@@ -1,5 +1,5 @@
 plot_exposure_lag <- 
-  function(results_list, groups, pollutant, colors, group_name) {
+  function(results_list, groups, pollutant, colors, group_name, filename) {
   # Extract the results for each group
   group_results <- lapply(groups, function(group) results_list[[group]][[pollutant]]$results_lag)
   
@@ -10,6 +10,9 @@ plot_exposure_lag <-
   df_results$group <- factor(rep(groups, each = nrow(group_results[[1]])), levels = groups)
   
   # Plot the data
+  # Open a JPEG device
+  jpeg(filename, width = 2500, height = 1500, res = 400, quality = 100)
+  plot.new()
   p <- ggplot(df_results, aes(x = lag, y = RR, color = group)) +
     geom_point(position = position_dodge(width = 0.4), size = 1) +
     geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.3, position = position_dodge(width = 0.4)) +
@@ -18,10 +21,12 @@ plot_exposure_lag <-
     scale_x_continuous(breaks = unique(df_results$lag)) +
     scale_color_manual(values = colors) +
     theme_minimal() + 
-    theme(panel.grid.major.x = element_line(color = "grey90"),
+    theme(panel.grid.major.x = element_line(color = "grey90", size = 0.2),
           panel.grid.minor.x = element_blank(),
-          panel.grid.major.y = element_line(color = "grey90"),  
-          panel.grid.minor.y = element_line(color = "grey90"))
+          panel.grid.major.y = element_line(color = "grey90", size = 0.2),  
+          panel.grid.minor.y = element_line(color = "grey90", size = 0.2))
   
   print(p)
+  # Close the JPEG device
+  dev.off()
 }
--- plot_exposure_response.R
+++ plot_exposure_response.R
@@ -1,8 +1,18 @@
-plot_exposure_response <- function(results_list, pollutant, groups, colors, ylim) {
+plot_exposure_response <- function(results_list, pollutant, groups, colors, ylim, filename) {
+  # Open a JPEG device
+  jpeg(filename, width = 2500, height = 1500, res = 400, quality = 100)
+  plot.new()
+  
+  par(mgp = c(2, 0.5, 0))
+  
   # Plot the overall predictions
   plot(results_list[[groups[1]]][[pollutant]]$meta_pred_tot, 'overall', lwd = 2, 
-       xlab = pollutant, ylab = 'RR', ylim = ylim, col = colors[1], 
-       ci.arg = list(density = 20, col = grey(0.2)))
+       xlab = pollutant, ylab = 'RR', ylim = ylim, col = colors[1], cex.lab = 0.8,
+       cex.axis = 0.8, ci.arg = list(density = 20, col = grey(0.2)))
+  
+  # Add grid
+  abline(h = pretty(ylim), col = "lightgrey", lty = 1, lwd = 0.5)
+  abline(v = pretty(par("usr")[1:2]), col = "lightgrey", lty = 1, lwd = 0.5)
   
   # Add the group predictions
   if (length(groups) > 1) {
@@ -14,7 +24,6 @@ plot_exposure_response <- function(results_list, pollutant, groups, colors, ylim
   
   # Add a legend
   legend("topright", legend = groups, col = colors, lwd = 2, bty = "n", cex = 0.7)
-  
-  # Add grid
-  grid()
+  # Close the JPEG device
+  dev.off()
 }
